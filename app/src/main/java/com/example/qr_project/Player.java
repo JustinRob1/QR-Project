febf8ff999c727069f64864e216787b4302e5205
Justin Robertson <jtrober1@ualberta.ca>
2023-02-28 19:18:23 -0700

    Added comments and javadocs

--- app/src/main/java/com/example/qr_project/Player.java
+++ app/src/main/java/com/example/qr_project/Player.java
@@ -4,13 +4,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Player {
-    private String username;
-    private String email;
-    private String userID;
-    private int phoneNumber;
-    private ArrayList<QR_Code> QRCodes;
-    private int totalScore;
+    private String username; // Stores the username
+    private String email; // Stores the users email
+    private String userID; // Stores the userID
+    private int phoneNumber; // Stores the phone number
+    private ArrayList<QR_Code> QRCodes; // Array list to store the QR codes
+    private int totalScore; // Stores the total scroe
 
+    /**
+     * Constructor for the player
+     * @param username
+     * @param email
+     * @param phoneNumber
+     * @param QRCodes
+     * @param totalScore
+     * @param userID
+     */
     public Player(String username, String email, int phoneNumber, ArrayList<QR_Code> QRCodes, int totalScore,
                     String userID) {
         this.username = username;
@@ -21,59 +30,107 @@ public class Player {
         this.userID = userID;
     }
 
+    /**
+     * @return
+     *      Username of the QR code
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * @return
+     *      Email of the user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email of the user
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @return
+     *      Phone number of the user
+     */
     public int getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Sets the user's phone number
+     * @param phoneNumber
+     */
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * @return
+     *      User's QR codes
+     */
     public ArrayList<QR_Code> getQRCodes() {
         return QRCodes;
     }
 
-    public void setQRCodes(ArrayList<QR_Code> QRCodes) {
-        this.QRCodes = QRCodes;
-    }
-
+    /**
+     * @return
+     *      Total score of all QR codes
+     */
     public int getTotalScore() {
         return totalScore;
     }
 
+    /**
+     * Sets the total score of the QR codes
+     * @param totalScore
+     */
     public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
     }
 
+    /**
+     * @return
+     *      User's ID
+     */
     public String getUserID() {
         return userID;
     }
 
+    /**
+     * Sets the user's ID
+     * @param userID
+     */
     public void setUserID(String userID) {
         this.userID = userID;
     }
 
+    /**
+     * Adds the qrCode to the array list and adds the QR code's score to total score
+     * @param qrCode
+     */
     public void addQrCode(QR_Code qrCode) {
         QRCodes.add(qrCode);
         totalScore = totalScore + qrCode.getScore();
     }
 
+    /**
+     * Deletes the qrCode from the array list and subtracts the QR code's score to total score
+     * @param qrCode
+     */
     public void deleteQrCode(QR_Code qrCode) {
         QRCodes.remove(qrCode);
         totalScore = totalScore - qrCode.getScore();
--- app/src/main/java/com/example/qr_project/QR_Code.java
+++ app/src/main/java/com/example/qr_project/QR_Code.java
@@ -1,15 +1,21 @@
 package com.example.qr_project;
 
-import android.provider.ContactsContract;
-
 public class QR_Code {
     // TODO
     // How to hash
-    private String hash;
-    private int score;
-    private String name;
-    private Object photo;
+    private String hash; // Stores the hash
+    private int score; // Stores the score of the QR code
+    private String name; // Stores the QR codes name
+    private Object photo; // Stores the photo taken
 
+    /**
+     * Constructor for QR Code
+     *
+     * @param hash
+     * @param score
+     * @param name
+     * @param photo
+     */
     public QR_Code(String hash, int score, String name, Object photo) {
         this.hash = hash;
         this.score = score;
@@ -17,34 +23,66 @@ public class QR_Code {
         this.photo = photo;
     }
 
+    /**
+     * @return
+     *      Hash of the QR code
+     */
     public String getHash() {
         return hash;
     }
 
+    /**
+     * Sets the hash
+     * @param hash
+     */
     public void setHash(String hash) {
         this.hash = hash;
     }
 
+    /**
+     * @return
+     *      Score of the QR Code
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Sets the score of the QR code
+     * @param score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * @return
+     *      Name of the QR code
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the QR code
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return
+     *      Photo of QR code
+     */
     public Object getPhoto() {
         return photo;
     }
 
+    /**
+     * Sets the photo
+     * @param photo
+     */
     public void setPhoto(Object photo) {
         this.photo = photo;
     }
