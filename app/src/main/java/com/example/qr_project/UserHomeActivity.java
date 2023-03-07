1268a677b27fffd30d92ba98cb0963867fb6b9f7
Justin Robertson <jtrober1@ualberta.ca>
2023-03-06 23:11:57 -0700

    Adding QR Scanner, Branched from Add_QR_Code2

--- app/src/main/java/com/example/qr_project/UserHomeActivity.java
+++ app/src/main/java/com/example/qr_project/UserHomeActivity.java
@@ -1,18 +1,13 @@
 package com.example.qr_project;
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import android.app.AlertDialog;
 
 // TODO: 1) Change layout from temporary once it's done
 //       2) Implement add QRCode
@@ -43,8 +38,6 @@ public class UserHomeActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
-
     }
 
     /**
