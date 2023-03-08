7f3e475345d23f640875a19df436d5de255c1295
Justin Robertson <jtrober1@ualberta.ca>
2023-03-08 15:39:26 -0700

    Fixed Main_Activity

--- app/src/main/java/com/example/qr_project/MainActivity.java
+++ app/src/main/java/com/example/qr_project/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,11 +14,5 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, UserHomeActivity.class);
         startActivity(intent);
-
-        // Write a message to database (this is a test for the firebase firestore)
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, FireStore FireBase is working now");
     }
 }
