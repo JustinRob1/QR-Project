7a7d5062e85fb6bb9c79fac601607c817e0d28ae
Abdullah Khadeli <akhadeli@ualberta.ca>
2023-03-12 12:04:43 -0600

    working on signup page

--- app/src/main/java/com/example/qr_project/activities/LandingPageActivity.java
+++ app/src/main/java/com/example/qr_project/activities/LandingPageActivity.java
@@ -1,20 +1,49 @@
 package com.example.qr_project.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.qr_project.R;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    Button signUpButton;
+    //FirebaseFirestore db;
+    ActivityResultLauncher<Intent> entryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
 
-        Intent intent = new Intent(this, UserHomeActivity.class);
-        startActivity(intent);
+        signUpButton = findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingPageActivity.this, SignUpActivity.class);
+                entryLauncher.launch(intent);
+            }
+        });
+        //db = FirebaseFirestore.getInstance();
+        //final CollectionReference collectionReference = db.collection("user");
+
+        //Intent intent = new Intent(this, UserHomeActivity.class);
+        //startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/qr_project/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.qr_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qr_project.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
