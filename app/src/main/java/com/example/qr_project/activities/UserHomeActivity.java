2afb4a02ba4f34c25152804d2ec1b5aed8707d29
Abdullah Khadeli <akhadeli@ualberta.ca>
2023-03-31 07:01:56 -0600

    setting up userhome top3 and userprofile top3

--- app/src/main/java/com/example/qr_project/activities/UserHomeActivity.java
+++ app/src/main/java/com/example/qr_project/activities/UserHomeActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -18,9 +19,13 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.ActivityCompat;
 
 import com.example.qr_project.R;
+import com.example.qr_project.utils.Hash;
+import com.example.qr_project.utils.QR_Adapter;
+import com.example.qr_project.utils.QR_Code;
 import com.example.qr_project.utils.UtilityFunctions;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -30,6 +35,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -43,20 +50,17 @@ public class UserHomeActivity extends AppCompatActivity {
     FirebaseFirestore db;
     String userId;
 
+    ArrayList<QR_Code> rankedQRCodes_list;
+    ArrayAdapter<QR_Code> rankedQRCodes_adapter;
+    ListView rankedQRCodes_view;
+
+    AppCompatButton viewAllBtn;
+
     TextView totalScore;
 
     TextView globalRank;
     TextView friendRank;
     TextView totalQrCodes;
-
-    TextView qrCode1Name;
-    TextView qrCode1Score;
-    TextView qrCode2Name;
-    TextView qrCode2Score;
-    TextView qrCode3Name;
-    TextView qrCode3Score;
-
-    ListView qrCodeTable;
 
     EditText searchUserTxt;
 
@@ -77,18 +81,17 @@ public class UserHomeActivity extends AppCompatActivity {
         CollectionReference collRef = db.collection("users");
         DocumentReference docRef = collRef.document(userId);
 
+        rankedQRCodes_list = new ArrayList<>();
+        rankedQRCodes_adapter = new QR_Adapter(this, rankedQRCodes_list);
+        rankedQRCodes_view = findViewById(R.id.user_top_qr_table);
+        rankedQRCodes_view.setAdapter(rankedQRCodes_adapter);
+
+        viewAllBtn = findViewById(R.id.view_all_btn);
+
         totalScore = findViewById(R.id.user_total_score);
         globalRank = findViewById(R.id.global_rank);
         friendRank = findViewById(R.id.friend_rank);
         totalQrCodes = findViewById(R.id.total_qr_codes);
-//        qrCode1Name = findViewById(R.id.qr_code_name_1);
-//        qrCode2Name = findViewById(R.id.qr_code_name_2);
-//        qrCode3Name = findViewById(R.id.qr_code_name_3);
-//        qrCode1Score = findViewById(R.id.qr_code_score_1);
-//        qrCode2Score = findViewById(R.id.qr_code_score_2);
-//        qrCode3Score = findViewById(R.id.qr_code_score_3);
-
-        qrCodeTable = findViewById(R.id.user_top_qr_table);
 
         searchUserTxt = findViewById(R.id.search_bar);
         searchUserBtn = findViewById(R.id.add_friend_btn);
@@ -176,15 +179,36 @@ public class UserHomeActivity extends AppCompatActivity {
                     if (data != null){
                         List<Map<String, Object>> qrCodes = (List<Map<String, Object>>) data.get("qrcodes");
                         if (qrCodes != null) {
+                            // Sort QR codes based on score
                             qrCodes.sort((a,b) -> ( (Long) b.get("score")).compareTo(( (Long) a.get("score")) ));
                             int rank = 1;
-                            for (Map<String, Object> qrCode : qrCodes) {
-                                String name = String.valueOf(qrCode.get("name"));
-                                Long score = (Long) qrCode.get("score");
 
+                            // Clear old version of the list
+                            rankedQRCodes_list.clear();
+
+                            for (Map<String, Object> qrCode : qrCodes) {
+                                if (rank > 3) break;
+
+                                // Creating QR_Code object for the adapter
+                                String name = String.valueOf(qrCode.get("name"));
+
+                                // POTENTIAL ERROR
+                                int score = Math.toIntExact((Long) qrCode.get("score"));
+                                String face = (String) qrCode.get("face");
+                                Hash hash = new Hash((String) qrCode.get("hash"), name, face, score);
+
+                                // adding QR_Code obj to the list
+                                rankedQRCodes_list.add(new QR_Code(hash, score, name, face));
 
                                 rank++;
                             }
+
+                            // Make view all button visible
+                            viewAllBtn.setVisibility(View.VISIBLE);
+
+                            // Notify adapter to update dataset
+                            rankedQRCodes_adapter.notifyDataSetChanged();
+
                             Log.d("DOCSNAP", qrCodes.toString());
                         }
                         totalScore.setText(String.valueOf(data.get("totalScore")));
---
+++ app/src/main/java/com/example/qr_project/utils/QR_Adapter.java
@@ -0,0 +1,49 @@
+package com.example.qr_project.utils;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.qr_project.R;
+
+import java.util.ArrayList;
+
+public class QR_Adapter extends ArrayAdapter<QR_Code> {
+
+    private ArrayList<QR_Code> qrCodes;
+    private Context context;
+
+    public QR_Adapter(Context context, ArrayList<QR_Code> qrCodes) {
+        super(context, 0, qrCodes);
+        this.qrCodes = qrCodes;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.user_qr_code_content, parent,false);
+        }
+
+        QR_Code qrCode = qrCodes.get(position);
+
+        TextView qrCodeNameTextView = view.findViewById(R.id.qr_code_name_1);
+        TextView scoreTextView = view.findViewById(R.id.qr_code_score_1);
+        TextView rankTextView = view.findViewById(R.id.qr_code_rank_1);
+
+        qrCodeNameTextView.setText(qrCode.getName());
+        scoreTextView.setText(String.valueOf(qrCode.getScore()));
+        String rank = (position + 1) +".";
+        rankTextView.setText(rank);
+
+        return view;
+    }
+}
