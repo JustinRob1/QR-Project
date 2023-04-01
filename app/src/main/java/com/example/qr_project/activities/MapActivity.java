808b48c4a568e59cd828d4ebdf9dd07745cc71f2
Justin Robertson <jtrober1@ualberta.ca>
2023-04-01 13:46:25 -0600

    Added Search Bar to Map

--- app/src/main/java/com/example/qr_project/activities/MapActivity.java
+++ app/src/main/java/com/example/qr_project/activities/MapActivity.java
@@ -6,13 +6,18 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +38,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -81,49 +87,76 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
 
                 // Set click listener for the marker
-                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                    @Override
-                    public boolean onMarkerClick(Marker clickedMarker) {
-                        Object tag = clickedMarker.getTag();
-                        if (tag != null && tag instanceof Map) {
-                            Map<String, Object> qrCode = (Map<String, Object>) tag;
-                            String photoUrl = qrCode.get("photo").toString();
-                            // Download the photo using Picasso and convert it to a Bitmap
-                            Picasso.get().load(photoUrl).into(new Target() {
-                                @Override
-                                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                                    // Show the custom view in an AlertDialog or other dialog
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(MapActivity.this);
-                                    View markerView = getLayoutInflater().inflate(R.layout.marker_layout, null);
-                                    ImageView imageView = markerView.findViewById(R.id.marker_image);
-                                    TextView textView = markerView.findViewById(R.id.marker_title);
-                                    Picasso.get().load(photoUrl).into(imageView);
-                                    textView.setText(Objects.requireNonNull(qrCode.get("name")).toString());
-                                    builder.setView(markerView);
-                                    builder.create().show();
-                                }
+                mMap.setOnMarkerClickListener(clickedMarker -> {
+                    Object tag = clickedMarker.getTag();
+                    if (tag != null && tag instanceof Map) {
+                        Map<String, Object> qrCode = (Map<String, Object>) tag;
+                        String photoUrl = qrCode.get("photo").toString();
+                        // Download the photo using Picasso and convert it to a Bitmap
+                        Picasso.get().load(photoUrl).into(new Target() {
+                            @Override
+                            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                                // Show the custom view in an AlertDialog or other dialog
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MapActivity.this);
+                                View markerView = getLayoutInflater().inflate(R.layout.marker_layout, null);
+                                ImageView imageView = markerView.findViewById(R.id.marker_image);
+                                TextView textView = markerView.findViewById(R.id.marker_title);
+                                Picasso.get().load(photoUrl).into(imageView);
+                                textView.setText(Objects.requireNonNull(qrCode.get("name")).toString());
+                                builder.setView(markerView);
+                                builder.create().show();
+                            }
 
-                                @Override
-                                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-                                    Log.e("My Tag", "Error downloading photo", e);
-                                }
+                            @Override
+                            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                                Log.e("My Tag", "Error downloading photo", e);
+                            }
 
-                                @Override
-                                public void onPrepareLoad(Drawable placeHolderDrawable) {
-                                    // Do nothing
-                                }
-                            });
-                        }
-                        return false;
+                            @Override
+                            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                                // Do nothing
+                            }
+                        });
                     }
+                    return false;
                 });
             } else {
                 Log.d("My Tag", "Error getting documents: ", task.getException());
             }
         });
 
-    }
+        // Initialize search bar
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Hide the keyboard after search query submitted
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
 
+                // Use Geocoder to get location information from search query
+                Geocoder geocoder = new Geocoder(MapActivity.this);
+                try {
+                    List<Address> addresses = geocoder.getFromLocationName(query, 1);
+                    if (!addresses.isEmpty()) {
+                        Address address = addresses.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    } else {
+                        Toast.makeText(MapActivity.this, "No results found for \"" + query + "\"", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
 
 
     @Override
