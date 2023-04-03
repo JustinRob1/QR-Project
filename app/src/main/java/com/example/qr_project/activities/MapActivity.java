478a0592808e6eebada40d6608c79fd63c443db8
Justin Robertson <jtrober1@ualberta.ca>
2023-04-02 20:44:39 -0600

    Implement Autocomplete Bar

--- app/src/main/java/com/example/qr_project/activities/MapActivity.java
+++ app/src/main/java/com/example/qr_project/activities/MapActivity.java
@@ -1,25 +1,18 @@
 package com.example.qr_project.activities;
 
-import static android.content.ContentValues.TAG;
-
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
-import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,14 +25,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.RectangularBounds;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.firestore.CollectionReference;
@@ -49,10 +38,6 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
-
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -65,55 +50,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private LocationManager mLocationManager;
     FirebaseFirestore db;
 
-    private PlacesClient placesClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        Places.initialize(getApplicationContext(), "AIzaSyBUux3nV7NYGBVtaRY4ZFmyppzqAm40zLU");
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Set the fields to return for the autocomplete results
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the selected place
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // Get the location of the selected place
+                LatLng latLng = place.getLatLng();
+
+                // Move the camera to the selected location
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            }
+
+            @Override
+            public void onError(Status status) {
+                // Handle the error
+            }
+        });
 
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         db = FirebaseFirestore.getInstance();
-
-        // Initialize Places API client
-        Places.initialize(getApplicationContext(), "AIzaSyBUux3nV7NYGBVtaRY4ZFmyppzqAm40zLU");
-        placesClient = Places.createClient(this);
-
-        // Set up autocomplete fragment container
-        AutocompleteSupportFragment autocompleteFragment = AutocompleteSupportFragment.newInstance();
-        autocompleteFragment.setPlaceFields(Arrays.asList(
-                Place.Field.ID,
-                Place.Field.NAME,
-                Place.Field.LAT_LNG
-        ));
-        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
-        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
-                new LatLngBounds(
-                        new LatLng(37.7749,-122.4194), // Southwest corner of San Francisco
-                        new LatLng(37.8199,-122.3548)  // Northeast corner of San Francisco
-                )
-        ));
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.autocomplete_container, autocompleteFragment)
-                .commit();
-
-        // Set up place selection listener
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                Log.i(TAG, "Place selected: " + place.getName() + ", " + place.getId() + ", " + place.getLatLng());
-                // Do something with the selected place
-            }
-
-            @Override
-            public void onError(@NonNull Status status) {
-                Log.e(TAG, "Error occurred: " + status.getStatusMessage());
-            }
-        });
 
         CollectionReference userRef = db.collection("users");
         userRef.get().addOnCompleteListener(task -> {
@@ -183,7 +155,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Log.d("My Tag", "Error getting documents: ", task.getException());
             }
         });
-
     }
 
 
